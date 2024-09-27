4394de1f82124e0768c8707ca56400addb5d82f5
leeyangy <liyangyang0713@foxmail.com>
2024-09-27 11:01:55 +0800

    fix(): add test res file
    at widget.cpp and widget.ui use QFile and QPixmap test res is corrected outpout in console and
    widget componse.

--- widget.cpp
+++ widget.cpp
@@ -11,6 +11,37 @@ Widget::Widget(QWidget* parent) :
 {
     ui->setupUi(this);
     setWindowTitle("--Clion QT5.12.9 Template-- 测试版  V1.0 24-0905     制作：LEEYANGY");
+
+    // 测试文件是否能被正确读取
+    QFile file = QFile(":/lang/test.txt");
+
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        qDebug() << "Failed to open file";
+    }
+    else
+    {
+        qDebug() << "File opened successfully";
+        const QString text = file.readAll();
+        qDebug() << text;
+        file.close();
+    }
+
+
+    QPixmap pixmap(":/image/404.png");
+    if (pixmap.isNull())
+    {
+        qDebug() << "Image not found!";
+    }
+    else
+    {
+        qDebug() << "Image loaded successfully!";
+        ui->picture->setPixmap(pixmap);
+    }
+
+    ui->picture->setPixmap(QPixmap(":/image/404.png"));
+
+
 }
 
 Widget::~Widget()
