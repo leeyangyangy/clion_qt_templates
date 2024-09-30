d96e9253baac0d6c0a7cfca0c2c6e701cf423735
leeyangy <liyangyang0713@foxmail.com>
2024-09-30 15:32:47 +0800

    build(🚀): add opencv

--- widget.cpp
+++ widget.cpp
@@ -9,35 +9,20 @@
 #include  <QSqlQuery>
 #include <QSqlError>
 
+#include <opencv2/highgui/highgui.hpp>
+
 Widget::Widget(QWidget* parent) :
     QWidget(parent), ui(new Ui::Widget)
 {
     ui->setupUi(this);
     setWindowTitle("--Clion QT5.12.9 Template-- 测试版  V1.0 24-0928     制作：LEEYANGY");
 
-    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-    db.setHostName("127.0.0.1");
-    db.setUserName("root");
-    db.setPassword("root");
-    db.setPort(3306);
-    db.setDatabaseName("test");
+    const cv::Mat image = cv::imread("E:/ROG-4k.jpg", 1);
+    // create image window named "My Image"
+    cv::namedWindow("My Image");
+    // show the image on window
+    cv::imshow("My Image", image);
 
-    if (!db.open())
-    {
-        QMessageBox::warning(this, "fail", db.lastError().text());
-        qDebug() << "Failed to connect to database";
-        return;
-    } else
-    {
-        qDebug() << "Connected to database";
-    }
-    QSqlQuery query;
-    query.exec("select * from user");
-    while (query.next())
-    {
-        qDebug() << query.value("name").toString();
-    }
-    db.close();
 }
 
 Widget::~Widget()
